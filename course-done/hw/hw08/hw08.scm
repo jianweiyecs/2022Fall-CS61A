a2ed4eacbfd9b88456249bd31a57c6dbd2c118a0
yejianwei <jianwei.yecs@outlook.com>
2025-03-31 22:06:23 +0800

    finish hw08

--- course-done/hw/hw08/hw08.scm
+++ course-done/hw/hw08/hw08.scm
@@ -1,7 +1,22 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (if (or (null? s) (null? (cdr s)))
+      #t
+      (and (<= (car s) (car (cdr s))) (ascending? (cdr s)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s))))
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (cond 
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+  ))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+  (if (null? s) s
+    (cons (car s)
+      (no-repeats (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
--- course-done/hw/hw08/tests/ascending.py
+++ course-done/hw/hw08/tests/ascending.py
@@ -7,51 +7,46 @@ test = {
         {
           'code': r"""
           scm> (ascending? '(1 2 3 4 5))  ; #t or #f
-          545654f52801dba9cbbe0347d265df09
-          # locked
+          #t
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (ascending? '(1 5 2 4 3))  ; #t or #f
-          8cd49ce066289d3b150d7b6108dda61e
-          # locked
+          #f
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (ascending? '(2 2))  ; #t or #f
-          545654f52801dba9cbbe0347d265df09
-          # locked
+          #t
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (ascending? '(1 2 2 4 3))  ; #t or #f
-          8cd49ce066289d3b150d7b6108dda61e
-          # locked
+          #f
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (ascending? '())  ; #t or #f
-          545654f52801dba9cbbe0347d265df09
-          # locked
+          #t
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
