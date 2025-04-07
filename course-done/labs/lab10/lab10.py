43e057d34d89f21939533d0ff78b4a9644e0b41e
yejianwei <jianwei.yecs@outlook.com>
2025-04-07 22:01:10 +0800

    finish lab10

--- course-done/labs/lab10/lab10.py
+++ course-done/labs/lab10/lab10.py
@@ -14,20 +14,21 @@ def calc_eval(exp):
     3
     """
     if isinstance(exp, Pair):
-        operator = ____________ # UPDATE THIS FOR Q2
-        operands = ____________ # UPDATE THIS FOR Q2
+        operator = exp.first        
+        operands = exp.rest        
         if operator == 'and': # and expressions
             return eval_and(operands)
         elif operator == 'define': # define expressions
             return eval_define(operands)
         else: # Call expressions
-            return calc_apply(___________, ___________) # UPDATE THIS FOR Q2
+            return calc_apply(calc_eval(operator), operands.map(calc_eval))    
     elif exp in OPERATORS:   # Looking up procedures
-        return OPERATORS[exp]
+            return OPERATORS[exp]
     elif isinstance(exp, int) or isinstance(exp, bool):   # Numbers and booleans
         return exp
-    elif _________________: # CHANGE THIS CONDITION FOR Q4
-        return _________________ # UPDATE THIS FOR Q4
+    elif exp in bindings:   # Looking up variables        
+        return bindings[exp]
+    
 
 def calc_apply(op, args):
     return op(args)
@@ -52,6 +53,11 @@ def floor_div(args):
     20
     """
     "*** YOUR CODE HERE ***"
+    res = args.first
+    while args.rest != nil:
+        res = res // args.rest.first
+        args = args.rest
+    return res
 
 scheme_t = True   # Scheme's #t
 scheme_f = False  # Scheme's #f
@@ -74,6 +80,13 @@ def eval_and(expressions):
     True
     """
     "*** YOUR CODE HERE ***"
+    curr, val = expressions, True
+    while curr is not nil:
+        val = calc_eval(curr.first)
+        if val is scheme_f:
+            return scheme_f
+        curr = curr.rest
+    return val
 
 bindings = {}
 
@@ -93,6 +106,8 @@ def eval_define(expressions):
     2
     """
     "*** YOUR CODE HERE ***"
+    bindings[expressions.first] = calc_eval(expressions.rest.first)
+    return expressions.first
 
 OPERATORS = { "//": floor_div, "+": addition, "-": subtraction, "*": multiplication, "/": division }
 
--- course-done/labs/lab10/tests/using_pair.py
+++ course-done/labs/lab10/tests/using_pair.py
@@ -5,7 +5,7 @@ test = {
     {
       'cases': [
         {
-          'answer': '444b726568312f6761d05afbc3ef8701',
+          'answer': "Pair('+', Pair(Pair('-', Pair(2, Pair(4, nil))), Pair(6, Pair(8, nil))))",
           'choices': [
             "Pair('+', Pair('-', Pair(2, Pair(4, Pair(6, Pair(8, nil))))))",
             "Pair('+', Pair(Pair('-', Pair(2, Pair(4))), Pair(6, Pair(8))))",
@@ -14,12 +14,12 @@ test = {
             'None of these'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'Write out the Python expression that returns a `Pair` representing the given expression: (+ (- 2 4) 6 8)'
         },
         {
-          'answer': '37670e4b00633084aa22d884c6c9326d',
+          'answer': '+',
           'choices': [
             '-',
             '+',
@@ -29,12 +29,12 @@ test = {
             'None of these'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'What is the operator of the call expression?'
         },
         {
-          'answer': 'f691e16231ded18eebfd3f4f5ef545cd',
+          'answer': 'p.first',
           'choices': [
             'p',
             'p.first',
@@ -43,7 +43,7 @@ test = {
             'p.first.rest'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': r"""
           If the `Pair` you constructed in the previous part was bound to the name `p`,
@@ -51,7 +51,7 @@ test = {
           """
         },
         {
-          'answer': 'df97a47b2518e72a265467bdb7e64aff',
+          'answer': 'p.rest',
           'choices': [
             'p',
             'p.first',
@@ -60,7 +60,7 @@ test = {
             'p.first.rest'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': r"""
           If the `Pair` you constructed was bound to the name `p`, 
@@ -68,7 +68,7 @@ test = {
           """
         },
         {
-          'answer': '47cc7a335c0fc0140c6aabcbbdbce2f6',
+          'answer': 'p.rest.first',
           'choices': [
             'p',
             'p.first',
@@ -77,12 +77,12 @@ test = {
             'p.first.rest'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How would you retrieve only the first operand?'
         },
         {
-          'answer': 'fe8dc26395203a70e6223893495ce598',
+          'answer': "Pair('-', Pair(2, Pair(4, nil)))",
           'choices': [
             "'-'",
             "'+'",
@@ -93,7 +93,7 @@ test = {
             'Pair(2, Pair(4, nil))'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'What is the first operand of the call expression (prior to evaluation)?'
         }
